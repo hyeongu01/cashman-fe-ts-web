3b4a47169204893fb50a354e419c43c493fbf6a6
hyeongu01 <cdy5212@gmail.com>
2026-04-17 14:52:25 +0900

    test: 네이버 로그인 리다이렉트 테스트

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,7 @@
 import { api } from "@/lib/api";
 
 export const getLoginUrl = (provider: string) => {
+  console.log(`${window.location.origin}/callback`);
   switch (provider) {
     case 'naver':
       return api(`/auth/naver/login?redirectUrl=${window.location.origin}/callback`);
